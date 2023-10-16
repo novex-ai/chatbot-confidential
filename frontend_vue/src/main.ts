8f86228fd75769fe53205c89deb703b15df2ad6b
Brad Ito <phlogisticfugu@gmail.com>
2023-10-16 12:05:11 -0700

    adding quasar ui framework vite plugin

--- frontend_vue/src/main.ts
+++ frontend_vue/src/main.ts
@@ -2,6 +2,9 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/material-icons/material-icons.css'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +13,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(Quasar, {
+    plugins: {}, // import Quasar plugins and add here
+})
 
 app.mount('#app')
--- frontend_vue/vite.config.ts
+++ frontend_vue/vite.config.ts
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: { transformAssetUrls }
+    }),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass'
+    })
   ],
   resolve: {
     alias: {
