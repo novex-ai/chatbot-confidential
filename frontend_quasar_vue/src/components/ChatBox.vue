c769f961d7d77deb1e75f682527538cc2717bbbe
Brad Ito <phlogisticfugu@gmail.com>
2023-10-17 10:45:55 -0700

    tweaking chatbox styling

--- frontend_quasar_vue/src/components/ChatBox.vue
+++ frontend_quasar_vue/src/components/ChatBox.vue
@@ -1,17 +1,41 @@
 <template>
-      <div style="width: 100%; max-width: 400px">
-        <q-chat-message
-          label="Sunday, 19th"
-        />
+    <div
+        class="chatbox q-pa-md q-gutter-md column flex-center bg-grey-2"
+    >
+        <div class="message-container q-pa-md q-ma-none bg-white">
+            <div
+                class="message-window"
+            >
+            <q-chat-message
+                label="Sunday, 19th"
+            />
   
-        <q-chat-message
-          :text="['hey, how are you?']"
-          sent
-          stamp="7 minutes ago"
-        />
-        <q-chat-message
-          :text="['doing fine, how are you?']"
-          stamp="4 minutes ago"
-        />
-      </div>
-  </template>
+            <q-chat-message
+                :text="['hey, how are you?']"
+                sent
+                stamp="7 minutes ago"
+            />
+            <q-chat-message
+                :text="['doing fine, how are you?']"
+                stamp="4 minutes ago"
+            />
+        </div>
+    </div>
+      
+    <div>Input</div>
+  </div>
+</template>
+
+<style lang="scss">
+.chatbox {
+    width: 100%;
+
+    .message-container {
+        width: 100%;
+        max-width: 600px;
+    }
+    .message-window {
+        width: 100%;
+    }
+}
+</style>
