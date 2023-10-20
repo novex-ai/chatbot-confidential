373ba12b7da8e17736106d2a422eacb47af37872
Brad Ito <phlogisticfugu@gmail.com>
2023-10-20 12:36:42 -0700

    adding env var dependency

--- backend_sanic/api/chat.py
+++ backend_sanic/api/chat.py
@@ -1,5 +1,11 @@
+import os
+
 from sanic import Blueprint, Request, Websocket
 
+
+APP_OLLAMA_HOST: str = os.environ.get("APP_OLLAMA_HOST", "")
+if not APP_OLLAMA_HOST:
+    raise Exception("APP_OLLAMA_HOST environment variable not set")
 
 bp = Blueprint("chat")
 
