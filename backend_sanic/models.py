4d6bcb010678e751e2dea62cf3be095b7be6a1b8
Brad Ito <phlogisticfugu@gmail.com>
2023-10-26 11:46:33 -0700

    using declarative mapped_column over Column

--- backend_sanic/models.py
+++ backend_sanic/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, String, Integer, func
+from sqlalchemy import DateTime, String, Integer, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,11 +10,11 @@ class FileUpload(Base):
     __tablename__ = "file_upload"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    uploaded_at = Column(DateTime, default=func.now(), nullable=False)
-    status = Column(String, server_default="UPLOADED", nullable=False)
-    raw_filename = Column(String, nullable=False)
-    stored_filename = Column(String, nullable=False)
-    size_bytes = Column(Integer, nullable=False)
+    uploaded_at = mapped_column(DateTime, default=func.now(), nullable=False)
+    status = mapped_column(String, server_default="UPLOADED", nullable=False)
+    raw_filename = mapped_column(String, nullable=False)
+    stored_filename = mapped_column(String, nullable=False)
+    size_bytes = mapped_column(Integer, nullable=False)
 
 
 metadata = Base.metadata
