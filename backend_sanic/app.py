4dfb94b031c86210ebc45fa078ff61243d61280a
Brad Ito <phlogisticfugu@gmail.com>
2023-10-26 11:16:47 -0700

    adding async database session

--- backend_sanic/app.py
+++ backend_sanic/app.py
@@ -1,10 +1,22 @@
 from sanic import Sanic
 from sanic_ext import Extend
 from .api import bp as api_bp
+from .db import inject_sqlalchemy_session, close_sqlalchemy_session
 
 app = Sanic("app")
 app.config.CORS_ORIGINS = ["http://localhost:9000"]
 Extend(app)
+
+
+@app.middleware("request")
+async def inject_session(request):
+    await inject_sqlalchemy_session(request)
+
+
+@app.middleware("response")
+async def close_session(request, response):
+    await close_sqlalchemy_session(request)
+
 
 app.blueprint(api_bp, url_prefix="/api")
 app.static("", "frontend_quasar_vue/dist/spa/", index="index.html")
---
+++ backend_sanic/db.py
@@ -0,0 +1,39 @@
+from contextvars import ContextVar
+import os
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+APP_POSTGRES_HOST = os.environ.get("APP_POSTGRES_HOST", "localhost")
+APP_POSTGRES_USER = os.environ.get("APP_POSTGRES_USER", "postgres")
+APP_POSTGRES_PASSWORD = os.environ.get("APP_POSTGRES_PASSWORD", "")
+APP_POSTGRES_DB = os.environ.get("APP_POSTGRES_DB", "postgres")
+
+from contextvars import ContextVar
+from typing import Any
+
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+connection_url = (
+    f"postgresql+asyncpg://{APP_POSTGRES_USER}:{APP_POSTGRES_PASSWORD}"
+    f"@{APP_POSTGRES_HOST}/{APP_POSTGRES_DB}"
+)
+
+async_engine = create_async_engine(connection_url, echo=True)
+
+_sessionmaker = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)  # type: ignore
+
+_base_model_session_ctx = ContextVar("session")  # type: ignore
+
+
+async def inject_sqlalchemy_session(request):
+    request.ctx.session = _sessionmaker()
+    request.ctx.session_ctx_token = _base_model_session_ctx.set(request.ctx.session)
+
+
+async def close_sqlalchemy_session(request):
+    if hasattr(request.ctx, "session_ctx_token"):
+        _base_model_session_ctx.reset(request.ctx.session_ctx_token)
+        await request.ctx.session.close()
