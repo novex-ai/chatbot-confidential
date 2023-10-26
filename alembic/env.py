31e576d6c877e1015cb971c02a5678392f7c89e7
Brad Ito <phlogisticfugu@gmail.com>
2023-10-26 09:48:22 -0700

    updating alembic env to read engine url consistently

--- alembic/env.py
+++ alembic/env.py
@@ -39,6 +39,7 @@ connection_url = (
     f"@{APP_POSTGRES_HOST}/{APP_POSTGRES_DB}"
 )
 print(f"{connection_url=}")
+config.set_main_option("sqlalchemy.url", connection_url)
 
 
 def run_migrations_offline() -> None:
@@ -53,7 +54,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url", connection_url)
+    url = config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -79,7 +80,10 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
 
         with context.begin_transaction():
             context.run_migrations()
