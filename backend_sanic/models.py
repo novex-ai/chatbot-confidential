c7adbc1a4768eaa27ee6ae10eb434566a5dcf7d1
Brad Ito <phlogisticfugu@gmail.com>
2023-10-26 09:49:11 -0700

    adding a FileUpload model and alembic migration

--- backend_sanic/models.py
+++ backend_sanic/models.py
@@ -1,5 +1,20 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, DateTime, String, Integer, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
+
+
+class FileUpload(Base):
+    __tablename__ = "file_upload"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    uploaded_at = Column(DateTime, default=func.now(), nullable=False)
+    status = Column(String, server_default="UPLOADED", nullable=False)
+    raw_filename = Column(String, nullable=False)
+    stored_filename = Column(String, nullable=False)
+    size_bytes = Column(Integer, nullable=False)
+
+
 metadata = Base.metadata
