4acf6a41082d985f03505e80a14f71020acad99e
Brad Ito <phlogisticfugu@gmail.com>
2023-11-08 09:32:42 -0800

    pre-load the embedding model

--- backend_sanic/app.py
+++ backend_sanic/app.py
@@ -2,10 +2,21 @@ from sanic import Sanic
 from sanic_ext import Extend
 from .api import bp as api_bp
 from .db import inject_sqlalchemy_session, close_sqlalchemy_session
+from .embeddings import get_embedding_model
 
 app = Sanic("app")
 app.config.CORS_ORIGINS = ["http://localhost:9000"]
 Extend(app)
+
+
+@app.before_server_start
+async def before_server_start(app):
+    """
+    ensure that each worker is ready to serve requests
+    https://sanic.dev/en/guide/basics/listeners.html
+    """
+    model = get_embedding_model()
+    assert model is not None
 
 
 @app.middleware("request")
